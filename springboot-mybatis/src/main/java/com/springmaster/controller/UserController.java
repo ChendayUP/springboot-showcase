ccd0edcd33704041ae35bde86db6930b6f0ef5f1
chenxixin <chenxixin>
2019-07-12 16:02:38 +0800

    springboot mybatis项目添加

---
+++ springboot-mybatis/src/main/java/com/springmaster/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.springmaster.controller;
+
+import com.springmaster.pojo.User;
+import com.springmaster.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/query")
+    public User queryName(String name) {
+        User user = userService.selectUserByName(name);
+        return user;
+    }
+
+    @RequestMapping("/selectAllUser")
+    public List<User> selectAllUser() {
+        return userService.selectAllUser();
+    }
+
+    @RequestMapping("/insert")
+    public List<User> insert(String name) {
+        User user = userService.selectUserByName(name);
+        if (user != null) {
+            return null;
+        }
+        userService.insertService(name);
+        return userService.selectAllUser();
+    }
+
+    @RequestMapping("/changemoney")
+    public List<User> changemoney() {
+        userService.changemoney();
+        return userService.selectAllUser();
+    }
+
+    @RequestMapping("/delete")
+    public String delete(int userId) {
+        userService.deleteService(userId);
+        return "OK";
+    }
+}
---
+++ springboot-mybatis/src/main/resources/data.sql
@@ -0,0 +1,68 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 182.92.82.103
+ Source Server Type    : MySQL
+ Source Server Version : 50173
+ Source Host           : 182.92.82.103
+ Source Database       : mmall
+
+ Target Server Type    : MySQL
+ Target Server Version : 50173
+ File Encoding         : utf-8
+
+ Date: 04/13/2017 22:04:18 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `product`
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `category_id` int(11) NOT NULL COMMENT '分类id,对应category表的主键',
+  `name` varchar(100) NOT NULL COMMENT '商品名称',
+  `subtitle` varchar(200) DEFAULT NULL COMMENT '商品副标题',
+  `price` decimal(20,2) NOT NULL COMMENT '价格,单位-元保留两位小数',
+  `stock` int(11) NOT NULL COMMENT '库存数量',
+  `status` int(6) DEFAULT '1' COMMENT '商品状态.1-在售 2-下架 3-删除',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `mmall_product`
+-- ----------------------------
+BEGIN;
+INSERT INTO `product` VALUES ('26', '100002', 'Apple iPhone 7 Plus (A1661) 128G 玫瑰金色 移动联通电信4G手机', 'iPhone 7，现更以红色呈现。', '6999.00', '9991', '1', null, '2017-04-13 21:45:41');
+COMMIT;
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户表id',
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '用户密码，MD5加密',
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `question` varchar(100) DEFAULT NULL COMMENT '找回密码问题',
+  `answer` varchar(100) DEFAULT NULL COMMENT '找回密码答案',
+  `role` int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name_unique` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `mmall_user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES ('1', 'admin', '427338237BD929443EC5D48E24FD2B1A', 'admin@happymmall.com', '13800138000', '问题', '答案', '1', '2016-11-06 16:56:45', '2017-04-04 19:27:36');
+COMMIT;
+SET FOREIGN_KEY_CHECKS = 1;
